4b03eabea56a04d14ffac4065a53d87a9f1c3aaf
Mark Chandler <github@chanchan.io>
2021-06-20 13:06:11 -0400

    refactor(types): add id and description to FeatureFlag

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,6 +1,10 @@
 export interface FeatureFlag {
+  /** The unique id of the feature flag. */
+  id: string
   /** The title of the feature flag. */
   title: string
+  /** The description of the feature flag. Optional. */
+  description?: string
   /** Whether the feature flag is enabled. */
   isEnabled: boolean
 }
