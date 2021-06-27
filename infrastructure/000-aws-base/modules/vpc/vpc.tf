405645348a8c2da313caeaacf95839d12f733cd0
David J. Felix <felix.davidj@gmail.com>
2021-06-27 11:36:11 -0500

    feat: split vpc out into new module wrapper
    - Move vpc call to a module so we can repeatedly for each region
    - Call repeatedly for each region
    - Remove east-1 to make sure we can re-enable it

---
+++ infrastructure/000-aws-base/modules/vpc/vpc.tf
@@ -0,0 +1,68 @@
+# Get a list of AZs supported by this region so we can put a subnet in each one
+data "aws_availability_zones" "this" {
+  filter {
+    name = "opt-in-status"
+
+    # This prevents local regions and outposts from being selected
+    values = ["opt-in-not-required"]
+  }
+}
+
+# Divide the subnets into groups and provide each group with a /18 space CIDR
+module "subnet_group_cidrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = var.cidr_block
+  networks = [
+    {
+      name     = "private"
+      new_bits = 2
+    },
+    {
+      name     = "public"
+      new_bits = 2
+    },
+  ]
+}
+
+# Divide the private groups by AZ and provide each with a /22 CIDR
+module "private_subnet_cidrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = module.subnet_group_cidrs.base_cidr_block
+  networks = [for az_name in sort(data.aws_availability_zones.this.names) : {name = az_name, new_bits = 4}]
+}
+
+# Divide the public groups by AZ and provide each with a /22 CIDR
+module "public_subnet_cidrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = module.subnet_group_cidrs.base_cidr_block
+  networks = [for az_name in sort(data.aws_availability_zones.this.names) : {name = az_name, new_bits = 4}]
+}
+
+locals {
+  subnet_count = length(data.aws_availability_zones.this.names)
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "base"
+  cidr = var.cidr_block
+
+  enable_ipv6                     = true
+  assign_ipv6_address_on_creation = true
+
+
+  azs             = sort(data.aws_availability_zones.this.names)
+  private_subnets = values(module.private_subnet_cidrs.network_cidr_blocks)
+  public_subnets  = values(module.public_subnet_cidrs.network_cidr_blocks)
+
+  private_subnet_ipv6_prefixes = range(local.subnet_count)
+  public_subnet_ipv6_prefixes	= range(local.subnet_count, local.subnet_count * 2)
+
+  enable_nat_gateway = true
+
+  tags = merge(var.tags)
+}
