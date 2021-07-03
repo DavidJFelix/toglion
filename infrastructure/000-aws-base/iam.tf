00b88288d553beb522cf047b0cd129110c595a81
David J. Felix <felix.davidj@gmail.com>
2021-07-03 14:13:47 -0500

    fix: issue with user paths and format

--- infrastructure/000-aws-base/iam.tf
+++ infrastructure/000-aws-base/iam.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_access_key" "terraform_cloud" {
-  user = aws_iam_user.terraform_cloud.name
+  user    = aws_iam_user.terraform_cloud.name
   pgp_key = "keybase:davidjfelix"
 }
 
 resource "aws_iam_access_key" "github_serverless" {
-  user = aws_iam_user.github_serverless.name
+  user    = aws_iam_user.github_serverless.name
   pgp_key = "keybase:davidjfelix"
 }
 
@@ -16,10 +16,10 @@ resource "aws_iam_group" "automation_cicd" {
 
 resource "aws_iam_user" "terraform_cloud" {
   name = "terraform-cloud"
-  path = "/automation/cicd/terraform-cloud"
+  path = "/automation/cicd/terraform-cloud/"
 }
 
 resource "aws_iam_user" "github_serverless" {
   name = "github-serverless"
-  path = "/automation/cicd/github-serverless"
+  path = "/automation/cicd/github-serverless/"
 }
