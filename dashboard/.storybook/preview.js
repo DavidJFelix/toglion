29b11e460f39bdf7927c01fa05a9cdea27d9035c
Mark Chandler <github@chanchan.io>
2021-07-18 13:16:48 -0400

    chore(storybook): add chakra decorator

--- dashboard/.storybook/preview.js
+++ dashboard/.storybook/preview.js
@@ -1,5 +1,18 @@
+import {ChakraProvider} from '@chakra-ui/react'
+import {StoryContext} from '@storybook/react'
+
+const withChakra = (StoryFn: Function, context: StoryContext) => {
+  return (
+    <ChakraProvider>
+      <StoryFn />
+    </ChakraProvider>
+  )
+}
+
+export const decorators = [withChakra]
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
