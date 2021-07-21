2c7fea15cbca391970485e071dae653f3ed52a25
David J. Felix <felix.davidj@gmail.com>
2021-07-21 16:09:58 -0500

    fix: add permissions to provisioned users

--- infrastructure/000-aws-base/iam.tf
+++ infrastructure/000-aws-base/iam.tf
@@ -8,10 +8,14 @@ resource "aws_iam_access_key" "github_serverless" {
   pgp_key = "keybase:davidjfelix"
 }
 
-
 resource "aws_iam_group" "automation_cicd" {
   name = "automation-cicd"
   path = "/automation/cicd/"
+}
+
+resource "aws_iam_group_policy_attachment" "automation_cicd_administrator" {
+  group      = aws_iam_group.automation_cicd.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_user" "terraform_cloud" {
@@ -19,7 +23,23 @@ resource "aws_iam_user" "terraform_cloud" {
   path = "/automation/cicd/terraform-cloud/"
 }
 
+resource "aws_iam_user_group_membership" "terraform_cloud" {
+  user = aws_iam_user.terraform_cloud.name
+
+  groups = [
+    aws_iam_group.automation_cicd.name,
+  ]
+}
+
 resource "aws_iam_user" "github_serverless" {
   name = "github-serverless"
   path = "/automation/cicd/github-serverless/"
+}
+
+resource "aws_iam_user_group_membership" "github_serverless" {
+  user = aws_iam_user.github_serverless.name
+
+  groups = [
+    aws_iam_group.automation_cicd.name,
+  ]
 }
