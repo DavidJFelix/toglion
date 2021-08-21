41af24fed422f8141244b8352cacdb7bc18530ea
David J. Felix <felix.davidj@gmail.com>
2021-08-21 18:29:31 -0500

    feat: add ssm params

--- infrastructure/010-aws-api/modules/serverless/ssm.tf
+++ infrastructure/010-aws-api/modules/serverless/ssm.tf
@@ -38,6 +38,18 @@ resource "aws_ssm_parameter" "global_dynamodb_table" {
   tags = merge(var.tags)
 }
 
+data "aws_dynamodb_table" "global_dynamodb_table_regional_replica" {
+  name = var.global_dynamodb_table
+}
+
+resource "aws_ssm_parameter" "global_dynamodb_stream_arn" {
+  name  = "/services/api/GLOBAL_DYNAMODB_STREAM_ARN"
+  type  = "String"
+  value = data.aws_dynamodb_table.global_dynamodb_table_regional_replica.stream_arn
+
+  tags = merge(var.tags)
+}
+
 resource "aws_ssm_parameter" "regional_dynamodb_table" {
   name  = "/services/api/REGIONAL_DYNAMODB_TABLE"
   type  = "String"
