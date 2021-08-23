50355c162b799a22c6305b6e244e162b4cf9e602
David J. Felix <felix.davidj@gmail.com>
2021-08-23 16:49:32 -0500

    fix: remove more output variables

--- infrastructure/000-aws-base/modules/vpc/outputs.tf
+++ infrastructure/000-aws-base/modules/vpc/outputs.tf
@@ -18,10 +18,6 @@ output "default_security_group_id" {
   value = module.vpc.default_security_group_id
 }
 
-output "dynamodb_vpc_endpoint_id" {
-  value = data.aws_vpc_endpoint.dynamodb.id
-}
-
-output "execute_api_vpc_endpoint_id" {
-  value = data.aws_vpc_endpoint.execute_api.id
-}
+# output "dynamodb_vpc_endpoint_id" {
+#   value = data.aws_vpc_endpoint.dynamodb.id
+# }
