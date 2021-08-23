4299cbe1c6cedc1d067786a9d75e499f2d349cf7
David J. Felix <felix.davidj@gmail.com>
2021-08-23 15:52:49 -0500

    fix: use public vpc subnets for serverless
    - Using VPC endpoints for these services doesn't work because of API Gateway.
    - The only alternative to public subnets is NAT gateway + private subnets:
      * doesn't provide any additional security since lambdas don't have inbound network listeners like SSH
      * Uses IPv4 EIPs which have a limit lower than our need
      * Has a passive cost

--- infrastructure/010-aws-api/serverless.tf
+++ infrastructure/010-aws-api/serverless.tf
@@ -5,7 +5,7 @@ module "us_east_1_serverless" {
   }
 
   role_arn                = aws_iam_role.lambda_role.arn
-  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_1_intra_subnets
+  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_1_public_subnets
   security_groups         = [data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_1_default_security_group_id]
   access_log_bucket       = data.terraform_remote_state.aws_base.outputs.aws_s3_access_logs_us_east_1_s3_bucket_id
   global_dynamodb_table   = module.global_dynamodb_table.dynamodb_table_id
@@ -20,7 +20,7 @@ module "us_east_2_serverless" {
   }
 
   role_arn                = aws_iam_role.lambda_role.arn
-  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_2_intra_subnets
+  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_2_public_subnets
   security_groups         = [data.terraform_remote_state.aws_base.outputs.aws_vpc_us_east_2_default_security_group_id]
   access_log_bucket       = data.terraform_remote_state.aws_base.outputs.aws_s3_access_logs_us_east_2_s3_bucket_id
   global_dynamodb_table   = module.global_dynamodb_table.dynamodb_table_id
@@ -35,7 +35,7 @@ module "us_west_1_serverless" {
   }
 
   role_arn                = aws_iam_role.lambda_role.arn
-  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_1_intra_subnets
+  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_1_public_subnets
   security_groups         = [data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_1_default_security_group_id]
   access_log_bucket       = data.terraform_remote_state.aws_base.outputs.aws_s3_access_logs_us_west_1_s3_bucket_id
   global_dynamodb_table   = module.global_dynamodb_table.dynamodb_table_id
@@ -50,7 +50,7 @@ module "us_west_2_serverless" {
   }
 
   role_arn                = aws_iam_role.lambda_role.arn
-  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_2_intra_subnets
+  subnets                 = data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_2_public_subnets
   security_groups         = [data.terraform_remote_state.aws_base.outputs.aws_vpc_us_west_2_default_security_group_id]
   access_log_bucket       = data.terraform_remote_state.aws_base.outputs.aws_s3_access_logs_us_west_2_s3_bucket_id
   global_dynamodb_table   = module.global_dynamodb_table.dynamodb_table_id
