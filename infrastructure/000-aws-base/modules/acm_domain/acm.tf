a2b6185104bd2c910f9faaefcbbab4ec4542c25d
David J. Felix <felix.davidj@gmail.com>
2021-08-28 17:04:28 -0500

    typo: fix another reference to main rather than this

--- infrastructure/000-aws-base/modules/acm_domain/acm.tf
+++ infrastructure/000-aws-base/modules/acm_domain/acm.tf
@@ -20,6 +20,6 @@ resource "aws_acm_certificate" "this" {
 }
 
 resource "aws_acm_certificate_validation" "this" {
-  certificate_arn         = aws_acm_certificate.main.arn
+  certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
 }
