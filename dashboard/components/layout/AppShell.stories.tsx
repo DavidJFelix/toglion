4bdb74a66ba1c2e742c7e4bb1c9946826ec9df2b
David J. Felix <felix.davidj@gmail.com>
2021-12-02 16:50:51 -0500

    feat: add basic app shell

---
+++ dashboard/components/layout/AppShell.stories.tsx
@@ -0,0 +1,29 @@
+import {Box} from '@chakra-ui/react'
+import React from 'react'
+import {AppShell} from './AppShell'
+
+function BlankAppShell() {
+  return (
+    <AppShell>
+      <Box
+        h="3xl"
+        borderStyle="dashed"
+        borderWidth={4}
+        borderRadius="base"
+        borderColor="gray.200"
+      />
+    </AppShell>
+  )
+}
+
+export default {
+  title: 'App Shell',
+  component: BlankAppShell,
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export const Empty = {
+  args: {},
+}
