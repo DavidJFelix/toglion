0755abc58a7c23821d35b7f6c852a2acdce93e62
David J. Felix <felix.davidj@gmail.com>
2021-12-11 09:42:08 -0600

    feat: deploy regional setups

--- infrastructure/001-aws-regional-base/src/index.ts
+++ infrastructure/001-aws-regional-base/src/index.ts
@@ -1,5 +1,5 @@
-import {Config} from '@pulumi/pulumi'
-import {iam, kms, route53, s3} from '@pulumi/aws'
+import {Config, getProject, getStack} from '@pulumi/pulumi'
+import {kms, s3} from '@pulumi/aws'
 
 interface AWSBaseData {
   tags: Record<string, string>
@@ -8,15 +8,17 @@ interface AWSBaseData {
 const config = new Config()
 const {tags} = config.requireObject<AWSBaseData>('data')
 
-export const rootLoggingKey = new kms.Key('root-logging', {
+// We'll use this key for lambda deployment including source encryption
+// as well as env variable encryption
+export const lambdaDeploymentKey = new kms.Key('lambda-deployment', {
   description: 'A kms key for AWS Access Logs',
   enableKeyRotation: true,
-  tags: {},
+  tags: {...tags},
 })
 
-export const rootLoggingKeyAlias = new kms.Alias('root-logging', {
-  namePrefix: 'alias/root-logging',
-  targetKeyId: rootLoggingKey.keyId,
+export const lambdaDeploymentKeyAlias = new kms.Alias('lambda-deployment', {
+  namePrefix: 'alias/lambda-deployment',
+  targetKeyId: lambdaDeploymentKey.keyId,
 })
 
 export const s3AccessLogBucket = new s3.Bucket('access-log', {
@@ -24,21 +26,37 @@ export const s3AccessLogBucket = new s3.Bucket('access-log', {
   bucketPrefix: 'access-log',
   serverSideEncryptionConfiguration: {
     rule: {
-      applyServerSideEncryptionByDefault: {
-        sseAlgorithm: 'aws:kms',
-        kmsMasterKeyId: rootLoggingKey.keyId,
-      },
-    },
-  },
-  tags: {},
-})
-
-export const lambdaDeploymentBucket = new s3.Bucket('lambda-deployment', {
-  serverSideEncryptionConfiguration: {
-    rule: {
+      // (2021-12-11) Log delivery buckets cannot be KMS encrypted
+      // The service account which does log delivery rotates and can't be granted
+      // kms permissions
       applyServerSideEncryptionByDefault: {
         sseAlgorithm: 'AES256',
       },
     },
+  },
+  tags: {...tags},
+})
+
+export const lambdaDeploymentBucket = new s3.Bucket('lambda-deployment', {
+  acl: 'private',
+  bucketPrefix: 'lambda-deployment',
+  loggings: [
+    {
+      targetBucket: s3AccessLogBucket.id,
+      targetPrefix: `aws/s3/${getProject()}/${getStack()}/lambda-deployment/`,
+    },
+  ],
+  serverSideEncryptionConfiguration: {
+    rule: {
+      applyServerSideEncryptionByDefault: {
+        kmsMasterKeyId: lambdaDeploymentKey.keyId,
+        sseAlgorithm: 'aws:kms',
+      },
+    },
+  },
+  tags: {...tags},
+  // Versioning makes this bucket easier operationally to see historic deployments
+  versioning: {
+    enabled: true,
   },
 })
