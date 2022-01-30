4b49bb3fea36183a4eee49a6a0a7f906bd13be30
with-heart <with.heart+git@pm.me>
2022-01-30 11:43:48 -0500

    chore(storybook): use @chakra-ui/storybook-addon

--- dashboard/.storybook/main.js
+++ dashboard/.storybook/main.js
@@ -1,28 +1,12 @@
-const path = require('path')
-
-const toPath = (_path) => path.join(process.cwd(), '..', _path)
-
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-next-router',
+    '@chakra-ui/storybook-addon',
   ],
   features: {
     previewCsfV3: true,
-  },
-  webpackFinal: (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
-        },
-      },
-    }
   },
 }
--- dashboard/.storybook/preview.js
+++ dashboard/.storybook/preview.js
@@ -1,16 +1,5 @@
 import {INITIAL_VIEWPORTS} from '@storybook/addon-viewport'
-import {ChakraProvider} from '@chakra-ui/react'
 import {RouterContext} from 'next/dist/shared/lib/router-context'
-
-const withChakra = (StoryFn, context) => {
-  return (
-    <ChakraProvider>
-      <StoryFn />
-    </ChakraProvider>
-  )
-}
-
-export const decorators = [withChakra]
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
