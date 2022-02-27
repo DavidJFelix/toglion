2bcaa1c35497a6d27db438d67bf5199f379b3a21
David J. Felix <felix.davidj@gmail.com>
2022-02-26 20:09:22 -0600

    feat: add flag services

--- dashboard/pages/api/flags/[flagId].ts
+++ dashboard/pages/api/flags/[flagId].ts
@@ -2,16 +2,16 @@ import {getSessionFromCookie} from 'lib/next-auth/dynamodb-adapter'
 import {loginRequiredMiddleware} from 'lib/next-auth/middleware'
 import {getQueryFirst} from 'lib/next/utils'
 import {NextApiRequest, NextApiResponse} from 'next'
-import {getOrganization} from 'services/organizations'
+import {getFlag} from 'services/flags'
 
 export async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const session = await getSessionFromCookie(req)
 
-    const organizationId = getQueryFirst(req.query, 'organizationId')! // TODO: handle odd case this doesn't exist
+    const flagId = getQueryFirst(req.query, 'flagId')! // TODO: handle odd case this doesn't exist
     try {
-      const organization = await getOrganization({
-        organizationId,
+      const organization = await getFlag({
+        flagId,
         requestingUserId: session.user.id,
       })
       return res.status(200).json(organization)
--- dashboard/services/flags.ts
+++ dashboard/services/flags.ts
@@ -3,6 +3,7 @@ import {DynamoDBDocument} from '@aws-sdk/lib-dynamodb'
 import {config} from 'lib/config'
 import {Flag, NewFlag} from 'types'
 import {ulid} from 'ulid'
+import {NotAuthorized, NotFound} from './errors'
 
 export interface CreateFlagParams {
   newFlag: NewFlag
@@ -68,3 +69,89 @@ export async function createFlag({
   // FIXME: handle case where transact fails
   return {id, ...newFlag}
 }
+
+export interface DeleteFlagParams {
+  flagId: string
+  requestingUserId: string
+}
+export async function deleteFlag({}: DeleteFlagParams): Promise<void> {
+  // TODO
+}
+
+export interface ListFlagParams {
+  organizationId: string
+  requestingUserId: string
+}
+export async function listFlags({
+  organizationId,
+  requestingUserId,
+}: ListFlagParams) {
+  const ddbClient = new DynamoDB({
+    credentials: {
+      accessKeyId: config.aws.accessKeyId,
+      secretAccessKey: config.aws.secretAccessKey,
+    },
+  })
+  const docDbClient = DynamoDBDocument.from(ddbClient)
+  const orgPromise = docDbClient.get({
+    TableName: config.dynamodb.organizations,
+    Key: {
+      id: organizationId,
+    },
+  })
+  const resultPromise = docDbClient.query({
+    TableName: config.dynamodb.flags,
+    IndexName: 'name-by-organization',
+    KeyConditionExpression: '#o = :o',
+    ExpressionAttributeNames: {'#o': 'organization'},
+    ExpressionAttributeValues: {':o': requestingUserId},
+  })
+
+  const [org, result] = await Promise.all([orgPromise, resultPromise])
+  if (org.Item!.ownerUserId !== requestingUserId) {
+    throw new NotAuthorized()
+  }
+
+  // FIXME: actually do real marshalling
+  return (result.Items ?? []) as Flag[]
+}
+
+export interface GetFlagParams {
+  flagId: string
+  requestingUserId: string
+}
+export async function getFlag({flagId, requestingUserId}: GetFlagParams) {
+  const ddbClient = new DynamoDB({
+    credentials: {
+      accessKeyId: config.aws.accessKeyId,
+      secretAccessKey: config.aws.secretAccessKey,
+    },
+  })
+  const docDbClient = DynamoDBDocument.from(ddbClient)
+  const flag = await docDbClient.get({
+    TableName: config.dynamodb.flags,
+    Key: {
+      id: flagId,
+    },
+  })
+
+  if (!flag.Item) {
+    throw new NotFound()
+  }
+
+  const organization = await docDbClient.get({
+    TableName: config.dynamodb.organizations,
+    Key: {
+      id: flag.Item.organizationId,
+    },
+  })
+
+  if (organization.Item!.ownerUserId !== requestingUserId) {
+    throw new NotAuthorized()
+  }
+
+  return flag.Item as Flag
+}
+
+export interface UpdateFlagParams {}
+export async function updateFlagParams({}: UpdateFlagParams) {}
