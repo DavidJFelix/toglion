6abda3f22d2c6b93ee1b50ceaf3c5d9d17621f67
David J. Felix <felix.davidj@gmail.com>
2022-03-20 12:20:51 -0500

    feat: add create flag drawer

--- dashboard/pages/o/[organizationName]/flags/index.tsx
+++ dashboard/pages/o/[organizationName]/flags/index.tsx
@@ -1,16 +1,12 @@
-import {DynamoDB} from '@aws-sdk/client-dynamodb'
-import {DynamoDBDocument} from '@aws-sdk/lib-dynamodb'
-import {Box, Button, Flex, Heading, HStack} from '@chakra-ui/react'
-// import {Box, Button, Input} from '@chakra-ui/react'
+import {Box, Button, Flex, Heading, useDisclosure} from '@chakra-ui/react'
+import {CreateFlagDrawer} from 'components/drawers/CreateFlagDrawer'
 import {FeatureFlagsList} from 'components/FeatureFlagsListV2'
 import {AppShell} from 'components/layout/AppShell'
-import {config} from 'lib/config'
 import {getSessionFromCookie} from 'lib/next-auth/dynamodb-adapter'
-import {useCreateFlag, useListFlags, useUpdateFlag} from 'lib/react-query/api'
+import {useListFlags} from 'lib/react-query/api'
 import {GetServerSidePropsContext, GetServerSidePropsResult} from 'next'
-// import {useState} from 'react'
 import {getOrganizationByName} from 'services/organizations'
-import {Flag, Organization} from 'types'
+import {Organization} from 'types'
 
 export interface FlagListByOrganizationNamePageProps {
   organization: Organization
@@ -19,24 +15,23 @@ export interface FlagListByOrganizationNamePageProps {
 export function FlagListByOrganizationNamePage({
   organization,
 }: FlagListByOrganizationNamePageProps) {
-  // const [flagName, setFlagName] = useState('New Flag')
-
   const {isLoading, data} = useListFlags(organization.id)
-  // const {mutate: createFlag} = useCreateFlag()
-  const {mutate: updateFlag} = useUpdateFlag()
-
-  const onFlagToggle = (newFlag: Flag) => {
-    // rq mutate - updateFlag(id, newValue)
-    updateFlag(newFlag)
-  }
+  const {isOpen, onClose, onOpen} = useDisclosure()
 
   return (
     <AppShell organizationName={organization.name}>
+      <CreateFlagDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        organization={organization}
+      />
       <Flex justifyContent="space-between">
         <Heading as="h1" size="lg" color="gray.800">
           Flags
         </Heading>
-        <Button colorScheme="orange">New flag</Button>
+        <Button colorScheme="orange" onClick={onOpen}>
+          New flag
+        </Button>
       </Flex>
       {!isLoading && data && (
         <Box mt={6}>
@@ -46,23 +41,6 @@ export function FlagListByOrganizationNamePage({
           />
         </Box>
       )}
-      {/* <Input
-        value={flagName}
-        onChange={({target: {value}}) => setFlagName(value)}
-      />
-      <Button
-        colorScheme="orange"
-        onClick={() =>
-          createFlag({
-            name: flagName,
-            value: true,
-            schema: JSON.stringify({type: 'boolean'}),
-            organizationId: organization.id,
-          })
-        }
-      >
-        Create Flag
-      </Button> */}
     </AppShell>
   )
 }
