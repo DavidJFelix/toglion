a55d481a9b2f83a0b73d038ce20b2241b7df84bf
David J. Felix <felix.davidj@gmail.com>
2022-04-21 07:59:12 -0500

    fix: unused imports and mut

--- event-gateway/src/sse.rs
+++ event-gateway/src/sse.rs
@@ -3,7 +3,7 @@ use axum::{
     response::sse::{Event, Sse},
     Extension,
 };
-use futures::stream::{self, Stream};
+use futures::stream::Stream;
 use std::{convert::Infallible, time::Duration};
 use tokio::sync::mpsc;
 use tokio_stream::{wrappers::UnboundedReceiverStream, StreamExt};
@@ -11,14 +11,13 @@ use tokio_stream::{wrappers::UnboundedReceiverStream, StreamExt};
 use crate::app::{Connection, ConnectionState, SSEConnectionState, ULIDGenerator};
 
 pub async fn sse_handler(
-    TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
     Extension(ulid_generator): Extension<ULIDGenerator>,
     Extension(connection_state): Extension<ConnectionState>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let connection_id = ulid_generator.lock().await.generate().unwrap();
 
     let (chan_responder, chan_requester) = mpsc::unbounded_channel();
-    let mut chan_requester_stream = UnboundedReceiverStream::from(chan_requester);
+    let chan_requester_stream = UnboundedReceiverStream::from(chan_requester);
 
     connection_state.write().await.insert(
         connection_id,
