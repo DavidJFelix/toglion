f848fd36f6d08b92cccabeca581d400245411214
David J. Felix <felix.davidj@gmail.com>
2022-08-14 14:34:56 -0500

    feat: reduce monthly spend

--- infrastructure/010-aws-regional-app/src/ecr.ts
+++ infrastructure/010-aws-regional-app/src/ecr.ts
@@ -4,13 +4,13 @@ import {regionalBaseVpc} from './stack-references'
 
 export const eventGatewayRepository = new awsx.ecr.Repository('event-gateway')
 
-export const eventGatewayLoadBalancer = new aws.alb.LoadBalancer(
-  'event-gateway',
-  {
-    subnets: regionalBaseVpc
-      ?.apply((vpc) => vpc.subnets)
-      .apply((subnets) => subnets.map((subnet) => subnet.id)),
-  },
-)
+// export const eventGatewayLoadBalancer = new aws.alb.LoadBalancer(
+//   'event-gateway',
+//   {
+//     subnets: regionalBaseVpc
+//       ?.apply((vpc) => vpc.subnets)
+//       .apply((subnets) => subnets.map((subnet) => subnet.id)),
+//   },
+// )
 
 export const eventGatewayCluster = new aws.ecs.Cluster('event-gateway')
